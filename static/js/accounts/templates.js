3ec77eabd35ff9bfc8ea911f16dcbc4373e69c70
Rhuan P <147956448+Rhuan-P-Dev@users.noreply.github.com>
2024-04-08 17:15:20 -0300

    refactor(accounts/template.js): translate to English

--- static/js/accounts/templates.js
+++ static/js/accounts/templates.js
@@ -1,19 +1,19 @@
 const accountTemplate = `<div class="account" id="{ID}" ready="False" type="???" actionLocked="locked">
-<input class="mw160px w15percentage" type="text" id="name" placeholder="Nome" title="Nome">
-<input class="required mw160px w15percentage" ready="False" type="number" id="value" placeholder="Valor" title="Valor">
-<input class="mw300px w50percentage" type="text" id="product" placeholder="Produto" title="Produto">
-<input class="mw160px w12percentage" type="text" id="method" placeholder="Método" title="Método">
-<input class="required mw35px w5percentage" ready="False" type="text" id="start" placeholder="Parcela Atual (Numérico ou INF)" title="Parcela Atual (Numérico ou INF)">
-<input class="required mw35px w5percentage" ready="False" type="text" id="end" placeholder="Parcela Final (Numérico ou INF)" title="Parcela Final (Numérico ou INF)">
-<input class="mw50px w5percentage" type="number" id="date" placeholder="Dia" title="Dia">
+<input class="mw160px w15percentage" type="text" id="name" placeholder="Name" title="Name">
+<input class="required mw160px w15percentage" ready="False" type="number" id="value" placeholder="Value" title="Value">
+<input class="mw300px w50percentage" type="text" id="product" placeholder="Product" title="Product">
+<input class="mw160px w12percentage" type="text" id="method" placeholder="Method" title="Method">
+<input class="required mw35px w5percentage" ready="False" type="text" id="start" placeholder="Current Installment (Numeric or INF)" title="Current Installment (Numeric or INF)">
+<input class="required mw35px w5percentage" ready="False" type="text" id="end" placeholder="Final Installment (Numeric or INF)" title="Final Installment (Numeric or INF)">
+<input class="mw50px w5percentage" type="number" id="date" placeholder="Day" title="Day">
 </div>`
 
 const accountTemplateFrezze = `<div class="account freeze" id="{ID}" ready="False" type="???">
-<input readonly type="text" id="name" placeholder="Nome" title="Nome">
-<input readonly class="required" ready="False" type="number" id="value" placeholder="Valor" title="Valor">
-<input readonly type="text" id="product" placeholder="Produto" title="Produto">
-<input readonly type="text" id="method" placeholder="Método" title="Método">
-<input readonly class="required" ready="False" type="text" id="start" placeholder="Parcela Atual (Numérico ou INF)" title="Parcela Atual (Numérico ou INF)">
-<input readonly class="required" ready="False" type="text" id="end" placeholder="Parcela Final (Numérico ou INF)" title="Parcela Final (Numérico ou INF)">
-<input readonly type="number" id="date" placeholder="Dia" title="Dia">
+<input readonly type="text" id="name" placeholder="Name" title="Name">
+<input readonly class="required" ready="False" type="number" id="value" placeholder="Value" title="Value">
+<input readonly type="text" id="product" placeholder="Product" title="Product">
+<input readonly type="text" id="method" placeholder="Method" title="Method">
+<input readonly class="required" ready="False" type="text" id="start" placeholder="Current Installment (Numeric or INF)" title="Current Installment (Numeric or INF)">
+<input readonly class="required" ready="False" type="text" id="end" placeholder="Final Installment (Numeric or INF)" title="Final Installment (Numeric or INF)">
+<input readonly type="number" id="date" placeholder="Day" title="Day">
 </div>`
