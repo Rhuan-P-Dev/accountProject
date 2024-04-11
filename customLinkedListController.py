eeb383c39d361b588534cd861c99332baa1974c0
Rhuan P <147956448+Rhuan-P-Dev@users.noreply.github.com>
2024-04-11 14:45:34 -0300

    refactor(CustomLinkedListC - thisMonthExist()): simplify

--- customLinkedListController.py
+++ customLinkedListController.py
@@ -173,16 +173,15 @@ class CustomLinkedListController:
         return False
 
     def thisMonthExist(self, year, month):
-        n = self.list["head"]
-        while True:
-            if(n["value"]["year"] == year):
-                for x in n["value"]["months"]:
+        node = self.list["head"]
+        while not node["next"] == "null":
+            if(node["value"]["year"] == year):
+                for x in node["value"]["months"]:
                     if(x["month"] == month):
                         return True
                 return False
-            if(n["next"] == "null"):
-                return False
-            n = n["next"]
+            node = node["next"]
+        return False
 
     def addMonth(self, year, month):
         n = self.list["head"]
